0667701286865642f78e0ab2a79d67089d23af52
matthew <mattmac02@hotmail.ca>
2021-08-23 14:18:02 -0400

    Finalized additional text box submission

--- index.js
+++ index.js
@@ -68,50 +68,63 @@ span.onclick = function () {
     modal1.style.display = "none";
 }
 
-// $(document).ready(function () {
-//     var wrapper1 = $(".infoFille"); //Fields wrapper
-//     var wrapper2 = $(".infoFillp"); //Fields wrapper
-//     var wrapper3 = $(".infoFillv"); //Fields wrapper
-//     var add_button1 = $(".submitButton2e"); //Add button ID
-//     var add_button2 = $(".submitButton2p"); //Add button ID
-//     var add_button3 = $(".submitButton2v"); //Add button ID
-//     var x = 1; //initlal text box count
-//     var y = 1;
-//     var z = 1;
-
-//     $(add_button1).click(function (e) { //on add input button click
-//         e.preventDefault();
-//         x++; //text box increment
-//         if (x == 2) {
-//             $(wrapper1).append('<div class="infoFille2"><h1>School 2</h1><div class="input_fields_wrap1"><input class="txtBox" type="text" name="School Location 2" size="100" placeholder="Where did you go to school?"> </div><div class="input_fields_wrap1"><label style="margin-right:18rem;color:#7395AE;">Start Date</label><label style="color:#7395AE;margin-right:3rem;">End Date</label><br><input class="txtBox1" type="date" name="School start date 2" placeholder="When did start and finish at this job?"><input type="date" class="txtBox1" name="School end date 2" ></div><div class="input_fields_wrap1"><input class="txtBox" type="text" name="School Awards " size="100" placeholder="Did you win any awards during your time at this school?"></div></div>');
-//         }//add input box
-//         else if (x == 3) {
-//             $(wrapper1).append('<div class="infoFille3"><h1>School 3</h1><div class="input_fields_wrap1"><input class="txtBox" type="text" name="School Location 3" size="100" placeholder="Where did you go to school?"> </div><div class="input_fields_wrap1"><label style="margin-right:18rem;color:#7395AE;">Start Date</label><label style="color:#7395AE;margin-right:3rem;">End Date</label><br><input class="txtBox1" type="date" name="School start date 3" placeholder="When did start and finish at this job?"><input type="date" class="txtBox1" name="School end date 3" ></div><div class="input_fields_wrap1"><input class="txtBox" type="text" name="School Awards " size="100" placeholder="Did you win any awards during your time at this school?"></div></div>');
-//         }
-//     });
-
-//     $(add_button2).click(function (e) {
-//         e.preventDefault();
-//         y++; //text box increment
-//         if (y == 2) {
-//             $(wrapper2).append('<form name="Customer Information" method="POST" data-netlify="true"><div class="infoFillp2"><h1>Experience 2</h1><div class="input_fields_wrap2"><input class="txtBox" type="text" name="Work Location 2" size="100" placeholder="Where did you work?"> </div><div class="input_fields_wrap2"><label style="margin-right:18rem;color:#7395AE;">Start Date</label><label style="color:#7395AE;margin-right:3rem;">End Date</label><br><input class="txtBox1" type="date" name="Work start date 2" placeholder="When did start and finish at this job?"><input type="date" class="txtBox1" name="Work end date 2" "></div><div class="input_fields_wrap2"><input class="txtBox" type="text" name="School Awards " size="100" placeholder="What was your job title and responsibilities at this company"></div></div></form>');
-//         }//add input box
-//         else if (y == 3) {
-//             $(wrapper2).append('<form name="Customer Information" method="POST" data-netlify="true"><div class="infoFillp3"><h1>Experience 3</h1><div class="input_fields_wrap2"><input class="txtBox" type="text" name="Work Location 3" size="100" placeholder="Where did you work?"> </div><div class="input_fields_wrap2"><label style="margin-right:18rem;color:#7395AE;">Start Date</label><label style="color:#7395AE;margin-right:3rem;">End Date</label><br><input class="txtBox1" type="date" name="Work start date 3" placeholder="When did start and finish at this job?"><input type="date" class="txtBox1" name="Work end date 3" ></div><div class="input_fields_wrap2"><input class="txtBox" type="text" name="School Awards " size="100" placeholder="What was your job title and responsibilities at this company?"></div></div></form>');
-//         }
-//     });
-//     $(add_button3).click(function (e) {
-//         e.preventDefault();
-//         z++; //text box increment
-//         if (z == 2) {
-//             $(wrapper3).append('<form name="Customer Information" method="POST" data-netlify="true"><div class="infoFillv2"><h1>Experience 2</h1><div class="input_fields_wrap3"><input class="txtBox" type="text" name="Extracurricular Experience 2" size="100" placeholder="What was the experience?"></div><div class="input_fields_wrap3"><label style="margin-right:18rem;color:#7395AE;">Start Date</label><label style="color:#7395AE;margin-right:3rem;">EndDate</label><br><input class="txtBox1" type="date" name="Volunteer start date 2" placeholder="When did start and finish at this school?"><input type="date" class="txtBox1" name="Volunteer end date 2"></div><div class="input_fields_wrap3"><input class="txtBox" type="text" name="School Awards " size="100" placeholder="What was your role and responsibilities in this experience?"></div></div></form>');
-//         }//add input box
-//         else if (z == 3) {
-//             $(wrapper3).append('<form name="Customer Information" method="POST" data-netlify="true"><div class="infoFillv3"><h1>Experience 3</h1><div class="input_fields_wrap3"><input class="txtBox" type="text" name="Extracurricular Experience 3" size="100" placeholder="What was the experience?"> </div><div class="input_fields_wrap3"><label style="margin-right:18rem;color:#7395AE;">Start Date</label><label style="color:#7395AE;margin-right:3rem;">EndDate</label><br><input class="txtBox1" type="date" name="Volunteer start date 3" placeholder="When did start and finish at this school?"><input type="date" class="txtBox1" name="Volunteer end date 3" ></div><div class="input_fields_wrap3"><input class="txtBox" type="text" name="School Awards " size="100" placeholder="What was your role and responsibilities in this experience?"></div></div></form>');
-//         }
-//     });
-// });
-document.body.onload = getElementById('id01');
+const targetDiv2 = document.getElementById("infoFille2");
+const targetDiv3 = document.getElementById("infoFille3");
+const btn1 = document.getElementById("submitButton2e");
+const btn2 = document.getElementById("submitButton2e2");
+const targetDiv22 = document.getElementById("infoFillp2");
+const targetDiv32 = document.getElementById("infoFillp3");
+const btn12 = document.getElementById("submitButton2p");
+const btn22 = document.getElementById("submitButton2p2");
+const targetDiv23 = document.getElementById("infoFillv2");
+const targetDiv33 = document.getElementById("infoFillv3");
+const btn13 = document.getElementById("submitButton2v");
+const btn23 = document.getElementById("submitButton2v2");
+targetDiv2.style.display = "none";
+targetDiv3.style.display = "none";
+targetDiv22.style.display = "none";
+targetDiv32.style.display = "none";
+targetDiv23.style.display = "none";
+targetDiv33.style.display = "none";
+btn1.onclick = function () {
+    if (targetDiv2.style.display == "none") {
+        targetDiv2.style.display = "block";
+        btn1.style.display = "none";
+        btn2.style.display = "block";
+    }
+}
+btn2.onclick = function () {
+    if (targetDiv3.style.display == "none") {
+        targetDiv3.style.display = "block";
+        btn2.style.display = "none";
+    }
+};
+btn12.onclick = function () {
+    if (targetDiv22.style.display == "none") {
+        targetDiv22.style.display = "block";
+        btn12.style.display = "none";
+        btn22.style.display = "block";
+    }
+}
+btn22.onclick = function () {
+    if (targetDiv32.style.display == "none") {
+        targetDiv32.style.display = "block";
+        btn22.style.display = "none";
+    }
+};
+btn13.onclick = function () {
+    if (targetDiv23.style.display == "none") {
+        targetDiv23.style.display = "block";
+        btn13.style.display = "none";
+        btn23.style.display = "block";
+    }
+}
+btn23.onclick = function () {
+    if (targetDiv33.style.display == "none") {
+        targetDiv33.style.display = "block";
+        btn23.style.display = "none";
+    }
+};
 
 
 
